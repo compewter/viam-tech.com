b6d1c20cb5f17e37369fa2339b135973ae5b4029
Michael W <michael@thecompewterlab.com>
2019-04-10 15:24:54 -0700

    Add browser router and prerendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom'
 import BlogFeed from './containers/BlogFeed'
 import BlogEntry from './containers/BlogEntry'
 import LossCalculator from './containers/LossCalculator'
@@ -18,27 +18,50 @@ const blogContentByPath = blogs.reduce((blogsByPath, blog)=>{
 class App extends Component {
   render() {
     return (
-     <HashRouter>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" render={props => <Home activePage='home' />} />
-            <Route exact path="/blog" render={props => <BlogFeed activePage='blog' />} />
-            <Route exact path="/services" render={props => <Services activePage='services' />} />
-            <Route exact path="/blog/:path" render={props => {
-              const blogContent = blogContentByPath[`/blog/${props.match.params.path}`]
-              if(blogContent){
-                return <BlogEntry activePage='blog' blog={blogContent} />
-              }
-              //eventually return not found content with recommended articles
-              return <BlogFeed activePage='blog' />
-            }}/>
-            <Route exact path="/contact" render={props => <Contact activePage='contact' />} />
-            <Route exact path="/calculator" render={props => <LossCalculator activePage='calculator' />} />
-            <Route exact path="/loss-calc-result" render={props => <LossCalcResult searchString={props.location.search} />} />
-            <Route render={props => <Home />} />
-          </Switch>
-        </div>
-      </HashRouter>
+      window.location.href.includes('/#/') ?
+        <HashRouter>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" render={props => <Home activePage='home' />} />
+              <Route exact path="/blog" render={props => <BlogFeed activePage='blog' />} />
+              <Route exact path="/services" render={props => <Services activePage='services' />} />
+              <Route exact path="/blog/:path" render={props => {
+                const blogContent = blogContentByPath[`/blog/${props.match.params.path}`]
+                if(blogContent){
+                  return <BlogEntry activePage='blog' blog={blogContent} />
+                }
+                //eventually return not found content with recommended articles
+                return <BlogFeed activePage='blog' />
+              }}/>
+              <Route exact path="/contact" render={props => <Contact activePage='contact' />} />
+              <Route exact path="/calculator" render={props => <LossCalculator activePage='calculator' />} />
+              <Route exact path="/loss-calc-result" render={props => <LossCalcResult searchString={props.location.search} />} />
+              <Route render={props => <Home />} />
+            </Switch>
+          </div>
+        </HashRouter>
+      :
+        <BrowserRouter>
+           <div className="App">
+             <Switch>
+               <Route exact path="/" render={props => <Home activePage='home' />} />
+               <Route exact path="/blog" render={props => <BlogFeed activePage='blog' />} />
+               <Route exact path="/services" render={props => <Services activePage='services' />} />
+               <Route exact path="/blog/:path" render={props => {
+                 const blogContent = blogContentByPath[`/blog/${props.match.params.path}`]
+                 if(blogContent){
+                   return <BlogEntry activePage='blog' blog={blogContent} />
+                 }
+                 //eventually return not found content with recommended articles
+                 return <BlogFeed activePage='blog' />
+               }}/>
+               <Route exact path="/contact" render={props => <Contact activePage='contact' />} />
+               <Route exact path="/calculator" render={props => <LossCalculator activePage='calculator' />} />
+               <Route exact path="/loss-calc-result" render={props => <LossCalcResult searchString={props.location.search} />} />
+               <Route render={props => <Home />} />
+             </Switch>
+           </div>
+         </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
+// ReactDOM.render(<App />, rootElement);
 registerServiceWorker();
